64e0e16c7e705df7594824875d4040a531130acb
Meetu <meetu@knoldus.com>
2013-03-19 12:58:18 +0530

    Added System Under Test and proper description

--- src/main/scala/com/knoldus/fixture/TaxCalculationFixture.scala
+++ src/main/scala/com/knoldus/fixture/TaxCalculationFixture.scala
@@ -1,15 +1,16 @@
 package com.knoldus.fixture
 
 import fitlibrary.DoFixture
+import com.knoldus.tax.TaxCalculatorService
 
+/**
+ * A Fixture acts as a bridge between Wiki Tests and System Under Test
+ */
 class TaxCalculationFixture extends DoFixture {
+  /**
+   * TaxCalculatorService is the System Under Test 
+   */
+  val taxCalculatorService = new TaxCalculatorService
 
-  def theTaxForAnnualIncomeIs(annualIncome: Double, tax: Double): Boolean = {
-    val calculatedTax = (annualIncome <= 100000.0) match {
-      case true => annualIncome * 0.10
-      case false => annualIncome * 0.15
-    }
-    calculatedTax == tax
-  }
-
+  def theTaxForAnnualIncomeIs(annualIncome: Double, tax: Double) = taxCalculatorService.getTaxForAnnualIncome(annualIncome) == tax
 }
---
+++ src/main/scala/com/knoldus/tax/TaxCalculatorService.scala
@@ -0,0 +1,12 @@
+package com.knoldus.tax
+
+class TaxCalculatorService {
+
+  /**
+   * If Annual Income is more than 100,000 tax is 15%. If Annual Income is less than equal to 100,000 tax is 10%
+   */
+  def getTaxForAnnualIncome(annualIncome: Double): Double = (annualIncome <= 100000.0) match {
+    case true => annualIncome * 0.10
+    case false => annualIncome * 0.15
+  }
+}
